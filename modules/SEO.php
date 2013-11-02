a4cabc2082c383e419b0b31b91c8d6ea16c75a70
boyevul <boyevul@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-02 14:44:46 +0000

    Keywords removed.
    git-svn-id: https://plugins.svn.wordpress.org/my-optional-modules/trunk@797728 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/SEO.php
+++ modules/SEO.php
@@ -1,23 +1,17 @@
 <?php
 
 	function mom_SEO_header() {
-	
+		global $post;	
 		if ( is_admin() ) {
 			function add_fields_to_profile($profile_fields) {
 				$profile_fields[ 'twitter_personal' ] = 'Twitter Username';
 				return $profile_fields;
 			}
 			function add_fields_to_general() {
-				register_setting( 'general', 'site_keywords', 'esc_attr' );
-				add_settings_field( 'site_keywords', '<label for="site_keywords">'.__('Keywords (comma separated)' , 'site_keywords' ).'</label>' , 'add_keywords_to_general_html', 'general' );
 				register_setting( 'general', 'site_twitter', 'esc_attr' );
 				add_settings_field( 'site_twitter', '<label for="site_twitter">'.__('Twitter Site username' , 'site_twitter' ).'</label>' , 'add_twitter_to_general_html', 'general' );				
 				
 			}			
-			function add_keywords_to_general_html() {
-				$keywords = get_option( 'site_keywords', '' );
-				echo '<textarea id="site_keywords" name="site_keywords">' . $keywords . '</textarea>';			
-			}
 			function add_twitter_to_general_html() {
 			$twitter = get_option( 'site_twitter', '');
 			echo '<input id="site_twitter" name="site_twitter" value="' . $twitter . '"/>';
@@ -27,77 +21,11 @@
 			add_filter( 'user_contactmethods', 'add_fields_to_profile');
 		}
 		
-
-		function extractCommonWords($string){
-			  $stopWords = array(
-			  'a','about','an','and','are','as','at','also','again','actually','actual',
-			  'b','be','by','because','being',
-			  'c','com','contrary',
-			  'd','de',
-			  'e','en','essentially','essential','even',
-			  'f','for','from','firstly','finally',
-			  'g',
-			  'h','how','however','hence',
-			  'i','in','is','it',
-			  'j','just',
-			  'k',
-			  'l','la','last','likewise',
-			  'm','most','more',
-			  'n','next','need',
-			  'o','of','on','or','only',
-			  'p','presently','primarily',
-			  'q',
-			  'r',
-			  's','secondly','similarily','since','should',
-			  't','that','the','this','to','thirdly','thus','their','there','than','then',
-			  'true',
-			  'they','them',
-			  'u','und','until',
-			  'v',
-			  'was','what','when','where','who','will','with','www','while','whereas',
-			  'wherever','whenever','whether','without','well','would',
-			  'x',
-			  'y','you','your','you\'re','you\'ll',
-			  'z',
-			  );
-		   
-			  $string = preg_replace('/\s\s+/i', '', $string);
-			  $string = trim($string);
-			  $string = preg_replace('/<(pre)(?:(?!<\/\1).)*?<\/\1>/s','',$string);
-			  $string = preg_replace('/\[.+\]/U', '', $string);
-			  $string = preg_replace('/[^a-zA-Z0-9 -]/', '', $string); 
-			  $string = preg_replace('/[0-9]/', '', $string);
-			  $string = strtolower($string); 
-		   
-			  preg_match_all('/\b.*?\b/i', $string, $matchWords);
-			  $matchWords = $matchWords[0];
-			  
-			  foreach ( $matchWords as $key=>$item ) {
-				  if ( $item == '' || in_array(strtolower($item), $stopWords) || strlen($item) <= 3 ) {
-					  unset($matchWords[$key]);
-				  }
-			  }   
-			  $wordCountArr = array();
-			  if ( is_array($matchWords) ) {
-				  foreach ( $matchWords as $key => $val ) {
-					  $val = strtolower($val);
-					  if ( isset($wordCountArr[$val]) ) {
-						  $wordCountArr[$val]++;
-					  } else {
-						  $wordCountArr[$val] = 1;
-					  }
-				  }
-			  }
-			  arsort($wordCountArr);
-			  $wordCountArr = array_slice($wordCountArr, 0, 10);
-			  return $wordCountArr;
-		}	
 	
 		function grab_keywords() {
 		
 				
 			// Keyword function http://www.hashbangcode.com/blog/extract-keywords-text-string-php-412.html
-			global $post;
 			$postid = $post->ID;
 			$authorID = $post->post_author;
 			$post_content = get_post_field('post_content', $postid);
@@ -105,24 +33,18 @@
 			$featured_image_url = $featured_image[0];
 			$post_link = get_permalink($post->ID);
 			$post_title = get_post_field('post_title', $postid);
-			$words = extractCommonWords($post_content);
-			$keywords = implode(',', array_keys($words));
 			$excerpt_from = get_post( $postid ); 
 			$the_excerpt = $excerpt_from->post_excerpt;
 			$description_content = get_bloginfo( 'description' );
 			$twitter_personal_content = get_the_author_meta( 'twitter_personal', $authorID );
 			$twitter_site_content = get_option( 'site_twitter' );
 			$site_keywords = get_option( 'site_keywords' );
-			$keywords_site_san = sanitize_text_field( $site_keywords );
-			$keywords_site_html = htmlentities( $keywords_site_san );
 			$post_title_final = htmlentities( $post_title );
 			$the_excerpt_final = htmlentities( $the_excerpt );
 					
 			if ( is_single() || is_page() ) {
 				if ( $description_content != "" )  echo "
 <meta name=\"description\" content=\"$description_content\"/>"; 
-				if ( $keywords != "" ) { echo "
-<meta name=\"keywords\" content=\"$keywords\"/>"; }
 				echo "
 <meta property=\"og:title\" content=\"$post_title_final\"/>";
 				if ( $featured_image_url != "" ) { echo "
@@ -144,8 +66,6 @@
 						}				
 				}
 			} else {
-				if ( $keywords_site_html != "" ) { echo "
-<meta name=\"keywords\" content=\"$keywords_site_html\"/>"; }
 			}
 			echo "
 			
