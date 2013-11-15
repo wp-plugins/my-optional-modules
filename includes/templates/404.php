5324133987d6a15530e4f716d612d29d2e947261
boyevul <boyevul@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-15 05:46:45 +0000

    * Code condensed * Many inconsequential errors fixed
    git-svn-id: https://plugins.svn.wordpress.org/my-optional-modules/trunk@804725 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/templates/404.php
@@ -0,0 +1,32 @@
+<?php if(!defined('MyOptionalModules')){ die('You can not call this file directly.');}$themetakeover_youtubeurl = get_option('MOM_themetakeover_youtubefrontpage');$themetakeoverfindvideoid = $themetakeover_youtubeurl;parse_str(parse_url($themetakeoverfindvideoid,PHP_URL_QUERY),$thevideoidforyoutube);$thevideoid = $thevideoidforyoutube['v'];$fitvids = plugins_url().'/my-optional-modules/includes/javascript/fitvids.js';?>
+<!DOCTYPE html>
+	<html <?php language_attributes();?>>
+	<head>
+		<meta charset="UTF-8" />
+		<title><?php wp_title( '|',true,'right');?></title>
+		<link rel="pingback" href="<?php bloginfo('pingback_url');?>" />
+		<link rel='stylesheet' id='MOMfourohfour-css'  href='<?php echo WP_PLUGIN_URL.'/my-optional-modules/includes/css/_404style.css';?>' type='text/css' media='all'/>
+		<?php wp_head();?>
+	</head>
+	<body <?php body_class();?>>
+		<div class="youtubethemetakeoveroverlay">
+			<h3>4 0 4</h3>
+			<form role="search" method="get" class="youtube404search" id="searchform" action="<?php echo esc_url(home_url('/'));?>">
+				<div>
+					<input type="text" value="" name="s" id="youtube404" placeholder="Search" /><input type="submit" id="yt404searchsubmit" value="Search" />
+				</div>
+			</form>
+			<div class="youtubethemetakeover">
+				<iframe src="http://www.youtube.com/embed/<?php echo esc_html($thevideoid);?>?autoplay=1&amp;loop=1&amp;playlist=<?php echo esc_html($thevideoid);?>&amp;controls=0&amp;showinfo=0&amp;autohide=1" frameborder="0" allowfullscreen></iframe>
+			</div>
+			<p>go <a href="<?php echo esc_url(home_url('/'));?>">home</a> - nothing to see here.</p>
+		</div>
+		<?php wp_footer();?>
+		<script type="text/javascript" src="<?php echo esc_html($fitvids);?>"></script>
+		<script>
+			jQuery(document).ready(function ($){
+				$(".youtubethemetakeover").fitVids();
+			});
+		</script>
+	</body>
+</html>
