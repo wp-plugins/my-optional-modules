80f0f6964815ed2c8efbc79adc9f8a1a3c12f833
boyevul <boyevul@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-10 18:30:57 +0000

    (bugfix) Missing folder (HTMLPurifier)
    git-svn-id: https://plugins.svn.wordpress.org/my-optional-modules/trunk@818393 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 Plugin Name: My Optional Modules
 Plugin URI: http://www.onebillionwords.com/my-optional-modules/
 Description: Optional modules and additions for Wordpress.
-Version: 5.3.9.9.5
+Version: 5.3.9.9.6
 Author: Matthew Trevino
 Author URI: http://onebillionwords.com 
 */
